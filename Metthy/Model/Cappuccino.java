63521b30c0606cf21019c911c03ac2dd7a18c4ca
Savie888 <queenie_mae_salao@dlsu.edu.ph>
2025-07-30 20:36:59 +0800

    DOCU DONE

--- Metthy/Model/Cappuccino.java
+++ Metthy/Model/Cappuccino.java
@@ -1,6 +1,18 @@
 package Metthy.Model;
 
+/**
+ * The Cappuccino class represents a coffee drink of type "Cappuccino".
+ * It extends the Drink class and sets its name and size upon creation.
+ */
+
 public class Cappuccino extends Drink{
+
+    /**
+     * Constructs a Cappuccino drink with the specified size.
+     * Calls the superclass constructor to initialize the name and size.
+     *
+     * @param size the size of the drink (e.g., "Small", "Medium", "Large")
+     */
 
     public Cappuccino(String size){
 
