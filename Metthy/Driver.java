04573a59362a2b75a3189cb9f80e86c2dbfc4bce
Metthy <adrian_matthew_dee@dlsu.edu.ph>
2025-07-30 21:35:57 +0800

    Create Driver.java

---
+++ Metthy/Driver.java
@@ -0,0 +1,29 @@
+package Metthy;
+
+
+import Metthy.Controller.MainController;
+
+/**
+ * This is a simulation of a Coffee Truck Business.
+ * <p>
+ * The application allows users to create and manage coffee trucks.
+ * Each truck maintains its own inventory, sales data, and drink preparation system.
+ * Users can perform actions such as:
+ * <ul>
+ *     <li>Create Regular or Special trucks</li>
+ *     <li>Have trucks prepare drinks</li>
+ *     <li>Track and display total sales revenue</li>
+ *     <li>Monitor ingredient usage and storage bins</li>
+ * </ul>
+ * <p>
+ * This class contains the main method to start the program.
+ * @author Adrian Matthew Dee and Queenie Salao
+ * @version 1.0
+ */
+public class Driver {
+
+    public static void main(String[] args){
+
+        MainController mainController = new MainController();
+    }
+}
